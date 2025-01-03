1acacd812a33bc55cc6b4bb87fe64a09bb8a8992
Oppossome <dsecutler@gmail.com>
2025-01-03 17:08:49 -0500

    Add SwaggerUI component

--- packages/sveltekit-openapi/src/lib/index.ts
+++ packages/sveltekit-openapi/src/lib/index.ts
@@ -1,2 +1,3 @@
 export { api } from "./api.js"
 export type { EndpointConfig, EndpointCallback } from "./api.types.js"
+export { default as SwaggerUI } from "./components/SwaggerUI.svelte"
--- packages/sveltekit-openapi/src/routes/+page.svelte
+++ packages/sveltekit-openapi/src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>SvelteKit OpenAPI Test App</h1>
+<script lang="ts">
+	import { SwaggerUI } from "sveltekit-openapi"
+</script>
+
+<SwaggerUI url="/api/todos/swagger.json" />
